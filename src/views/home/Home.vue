2179e9cd6ab0893923f5b988d6f73b82f8b80750
Taifendesune <1970024754@qq.com>
2020-08-29 15:53:52 +0800

    推荐

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,12 +6,14 @@
       </template>
     </nav-bar>
     <home-swiper :swipers="swipers"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar';
 import HomeSwiper from './childComponent/HomeSwiper';
+import RecommendView from './childComponent/RecommendView';
 
 import * as network from 'network/home';
 
@@ -26,7 +28,12 @@ export default {
         {href: 'http://www.bilibili.com'},
         {href: 'http://www.bilibili.com'},
       ],
-      recommends: null
+      recommends: [
+        {id: 1, title: '战斗法师', href: 'http://www.bilibili.com', src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598696669877&di=c8a748eeccc1f076b803fcc92ad3ca6a&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201703%2F12%2F20170312180706_dxnUW.jpeg'},
+        {id: 2, title: '元素师', href: 'http://www.bilibili.com', src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598697530905&di=9d9fd7c9f2aa74b563bed62feb405799&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F6b9d10fd9300bb0f0a4df9cd3b180b593ff528ad.jpg'},
+        {id: 3, title: '魔道学者', href: 'http://www.bilibili.com', src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2664117381,3770808857&fm=15&gp=0.jpg'},
+        {id: 4, title: '召唤师', href: 'http://www.bilibili.com', src: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3503917445,2136004486&fm=15&gp=0.jpg'}
+      ]
     };
   },
   created () {
@@ -38,7 +45,8 @@ export default {
   },
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView
   }
 };
 </script>
---
+++ src/views/home/childComponent/RecommendView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="recommends">
+    <section v-for="item in recommends" :key="item.id" class="recommend-item">
+      <a :href="item.href">
+        <img :src="item.src" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecommendView',
+  props: {
+    recommends: {
+      type: Array,
+      default () {
+        return [];
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  @import url(recommend-view.css);
+</style>
